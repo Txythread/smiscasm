45c0bd027a9e3b91c8acb1c1539b2d2bd552543a
Txythread <txyzthread@icloud.com>
2025-08-17 10:10:13 +0200

    Added jmp to register

--- test_code/test_linker.s
+++ test_code/test_linker.s
@@ -2,10 +2,20 @@
 main:
 	adrp x1, msg1@PAGE
 	add x1, msg1@PAGEOFF
+	out x1
 	adrp x2, msg2@PAGE
 	add x2, msg2@PAGEOFF
+	out x2
+	adrp x3, code@PAGE
+	add x3, code@PAGEOFF
+	jmp x3
 msg1:
 	.ascii "H"
+.section "CODE2"
+code:
+	adrp x2, main@PAGE
+	add x2, main@PAGEOFF
+	jmp main@PAGE
 .section "DATA"
 msg2:
 	.ascii "W"
