3371767b94da34fecee3015381b2158e8ed16a4a
Txythread <txyzthread@icloud.com>
2025-08-19 14:51:11 +0200

    Added production script

---
+++ production.sh
@@ -0,0 +1,17 @@
+# Make a production build and store it in the /usr/local/bin
+cargo build --release 1>/dev/null 2>/dev/null
+
+if [ $? -ne 0 ]; then
+	echo "Build failed!" 1>&2
+	exit 1
+fi
+
+
+cargo test 1>/dev/null 2>/dev/null
+
+if [ $? -ne 0 ]; then
+	echo "Tests failed!" 1>&2
+	exit 2
+fi
+
+sudo mv target/release/smiscasm /usr/local/bin
