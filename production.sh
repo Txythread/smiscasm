59f39da421fd346bf1a7ab0f8aa5e6c4c77ca7d3
Txythread <txyzthread@icloud.com>
2025-09-17 11:40:26 +0200

    Update production.sh

--- production.sh
+++ production.sh
@@ -1,4 +1,14 @@
 # Make a production build and store it in the /usr/local/bin
+
+cargo --version 1>/dev/null
+
+if [ $? -ne 0 ]; then
+	echo "Couldn't ivoke cargo. Is rust installed correctly?!" 1>&2
+	exit 1
+fi
+
+
+
 cargo build --release 1>/dev/null 2>/dev/null
 
 if [ $? -ne 0 ]; then
